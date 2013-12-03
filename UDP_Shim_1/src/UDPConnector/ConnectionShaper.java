b9b2cd83bdc614beef4aab31d03f2d6dc005f6fd
Jan Horjus <jmhorjus@gmail.com>
2013-12-03 01:52:55 -0500

    Some initial shaper code, still incomplete

--- UDP_Shim_1/src/UDPConnector/ConnectionShaper.java
+++ UDP_Shim_1/src/UDPConnector/ConnectionShaper.java
@@ -4,10 +4,51 @@
  */
 package UDPConnector;
 
+import java.net.InetAddress;
+import java.util.List;
+
 /**
  *
  * @author jhorjus
  */
-public class ConnectionShaper {
+public class ConnectionShaper implements ConnectorInterface
+{
+    ConnectorInterface m_connectionToShape;
+    int m_sendDelayInMs;
+    int m_receiveDelayInMs;
+    int packetsToDropPer10000;
+    
+    
+    ConnectionShaper(
+            ConnectorInterface connectionToShape, 
+            int sendDelayInMs, 
+            int receiveDelayInMs,
+            int packetsToDropPer10000)
+    {
+        m_connectionToShape = connectionToShape;
+    }
+    
+    
+    public void SetPeerAddress(
+        InetAddress peerAddress, 
+        int port)
+    {
+        m_connectionToShape.SetPeerAddress(peerAddress, port);
+    }
+            
+    public List<byte[]> Receive(int maxBlockingTimeInMs) throws Exception
+    {
+        return m_connectionToShape.Receive(maxBlockingTimeInMs);
+    }
+    
+    public boolean Send(String sendString) throws Exception
+    {
+        return m_connectionToShape.Send(sendString);
+    }
+    
+    public boolean Send(byte[] sendBuffer) throws Exception
+    {
+        return m_connectionToShape.Send(sendBuffer);
+    }
     
 }
