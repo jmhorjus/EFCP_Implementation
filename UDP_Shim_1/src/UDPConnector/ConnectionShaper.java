00d289096210b9db4f084b6d29ceaf255f3ec1e7
jmhorjus <jancomx@yahoo.com>
2013-12-05 09:13:38 -0500

    Lots of improvements to shaper implementation
    still not fully operational

--- UDP_Shim_1/src/UDPConnector/ConnectionShaper.java
+++ UDP_Shim_1/src/UDPConnector/ConnectionShaper.java
@@ -4,8 +4,10 @@
  */
 package UDPConnector;
 
+import static java.lang.Thread.sleep;
 import java.net.InetAddress;
 import java.util.List;
+import java.util.Random;
 
 /**
  *
@@ -15,17 +17,20 @@ public class ConnectionShaper implements ConnectorInterface
 {
     ConnectorInterface m_connectionToShape;
     int m_sendDelayInMs;
-    int m_receiveDelayInMs;
-    int packetsToDropPer10000;
+    int m_sendJitterInMs;
+    int m_packetsToDropPer10000;
     
     
     ConnectionShaper(
             ConnectorInterface connectionToShape, 
             int sendDelayInMs, 
-            int receiveDelayInMs,
+            int sendJitterInMs,
             int packetsToDropPer10000)
     {
         m_connectionToShape = connectionToShape;
+        m_sendDelayInMs = sendDelayInMs;
+        m_sendJitterInMs = sendJitterInMs;
+        m_packetsToDropPer10000 = packetsToDropPer10000;
     }
     
     
@@ -53,5 +58,79 @@ public class ConnectionShaper implements ConnectorInterface
     {
         return m_connectionToShape.Send(sendBuffer);
     }
+    
+    
+    
+    private class ShapedSendTask implements Runnable
+    {
+        String m_stringToSend;
+        byte[] m_bufferToSend;
+        boolean m_sendBuffer;
+        
+        ShapedSendTask(String stringToSend)
+        {
+            m_stringToSend = stringToSend;
+            m_sendBuffer = false;
+        }
+        ShapedSendTask(byte[] bufferToSend)
+        {
+            m_bufferToSend = bufferToSend;
+            m_sendBuffer = true;
+        }
+        
+        
+        @Override
+        public void run()
+        {
+            Random r = new Random();
+            int jitter = r.nextInt(m_sendJitterInMs);
+            try{
+            sleep(m_sendDelayInMs + jitter);
+            }
+            catch (Exception ex){
+                System.out.print("Exception caught while waiting in shaped send.");
+            }
+            
+            boolean dropPacket = (r.nextInt(10000) <= m_packetsToDropPer10000);
+            
+            if(!dropPacket)
+            {
+                try
+                {
+                if(m_sendBuffer)
+                    m_connectionToShape.Send(m_bufferToSend);
+                else
+                    m_connectionToShape.Send(m_stringToSend);
+                }
+                catch{
+                    System.out.print("Shaped send: Exception caught while sending.");
+                }
+            }
+        }
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
 }
